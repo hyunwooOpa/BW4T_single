dcb04f769cb7a67164acf6601c3d276af51651c7
Flinnmen <vidsflinn@gmail.com>
2024-12-08 16:49:02 +0100

    added up to 4.3 c

--- BW4T/bw4tKnowledge.pl
+++ BW4T/bw4tKnowledge.pl
@@ -4,6 +4,12 @@
 	state/1,	% percept predicate to keep track of whether a robot is moving.
 	visited/1,	% predicate to keep track of the rooms that have been visited.
 	zone/5.		% percept predicate with information about all zones in BW4T.
+	sequence/1.
+	sequenceIndex/1.
+	color/2.
+	atBlock/1.
+	holdingblocks/1.
+	block/3.
 
 % A room is a place with exactly one neighbour; in other words, there is only one way to
 % get to and from that place.
@@ -11,3 +17,6 @@ room(PlaceID) :- zone(_, PlaceID, _, _, Neighbours), length(Neighbours, 1).
 
 % A room has not yet been visited if the robot has not yet been there.
 not_yet_visited(PlaceID) :- room(PlaceID), not( visited(PlaceID) ).
+
+%Get the next color that is needed.
+next_color_needed(ColorID) :- sequenceIndex(Index), sequence(List), nth0(Index, List, ColorID).
